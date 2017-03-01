47061358cb2f55d34092a74fcdddb7e2879041e9
Twitchkidd <field.gareth@gmail.com>
2017-03-01 17:58:57 -0500

    eh, almost

--- src/Endzone.js
+++ src/Endzone.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import './Endzone.css';
 
 class Endzone extends Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         return(
             <div>
